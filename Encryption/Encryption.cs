19368a91175bab937e569e831c07109582c72569
ghalib5000 <ghalibahmed5000@gmail.com>
2020-02-18 03:31:53 +0500

    basic one with encryption

---
+++ Encryption/Encryption.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vigenere_Cipher.Encryption
+{
+    class Encryption
+    {
+        private string key;
+        private string decrypted_text;
+        private char[] decryptedtext;
+        private List<char> newkey = new List<char>();
+        List<int> combinedvals = new List<int>();
+        //these store the ascii numbers of characters
+
+        private List<int> keyvals = new List<int>();
+        private List<int> textvals = new List<int>();
+
+        string encryptedText ="";
+
+        alphabets alpha = new alphabets();
+        
+        public Encryption(string encrypted_text, string key)
+        {
+            //this.key = key;
+            this.key = key.ToUpper();
+            //this.decrypted_text = encrypted_text;
+            this.decrypted_text =encrypted_text.ToUpper();
+        }
+        public void startEncrypting()
+        {
+            supplyValues();
+            getkeyvals(newkey);
+            gettextvals(decryptedtext);
+            applyEncryption();
+            convertBackToString(combinedvals);     
+        }
+
+
+
+        public void getkeyvals(List<char> keys)
+        {
+            foreach (char c in keys)
+            {
+                keyvals.Add(Convert.ToInt32(c));
+            }
+        }
+        public void gettextvals(char[] text)
+        {
+            foreach (char c in text)
+            {
+                textvals.Add(Convert.ToInt32(c));
+            }
+        }
+        public void supplyValues()
+        {
+            decryptedtext = decrypted_text.ToCharArray();
+            int j = 0;
+            for (int i = 0; i < decryptedtext.Length; i++)
+            {
+                if (j == key.Length)
+                {
+                    j = 0;
+                }
+                newkey.Add(key[j]);
+                j++;
+            }
+
+        }
+
+        public void applyEncryption()
+        {
+
+            for (int i = 0; i < decryptedtext.Length; i++) {
+                int temp,texttemp;
+                temp = alpha.aphabets.FindIndex(0, x => x.Equals(newkey[i]));
+                texttemp = alpha.aphabets.FindIndex(0, x => x.Equals(decrypted_text[i]));
+                int newtemp = temp + texttemp;
+                if(newtemp>=26)
+                {
+                  newtemp=newtemp - 26;
+                }
+                encryptedText += alpha.aphabets[newtemp];
+            }
+
+        }
+        public string returnencryption()
+        {
+            return encryptedText;
+        }
+        public void convertBackToString(List<int> list)
+        {
+            foreach(int i in list)
+            {
+                char c = Convert.ToChar(i);
+                encryptedText += c;
+            }
+        }
+    }
+}
---
+++ alphabets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vigenere_Cipher
+{
+    class alphabets
+    {
+        public List<char> aphabets = new List<char> {
+            'A','B','C','D','E','F','G','H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+            'S','T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a','b','c','d','e','f','g','h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's','t', 'u', 'v', 'w', 'x', 'y', 'z', };
+    }
+}
